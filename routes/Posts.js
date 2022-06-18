9d4f78c3cb5f2ddb5385b19b64e8c6b3c1c78181
Hirano <hnaoya1120@gmail.com>
2022-06-18 16:59:04 +0900

    投稿詳細画面のendpoint

--- routes/Posts.js
+++ routes/Posts.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const {Posts} = require("../models");
 
 router.get("/", async (req, res) => {
-    // SELECT * from Posts;
+    // SELECT * FROM Posts;
     const listOfPosts = await Posts.findAll()
     res.json(listOfPosts);
+});
+
+router.get("/byId/:id", async (req, res) => {
+    const id = req.params.id;
+    // "SELECT * FROM Posts WHERE id = ?"
+    const post = await Posts.findByPk(id);
+    res.json(post);
 });
 
 router.post("/", async (req, res) => {
