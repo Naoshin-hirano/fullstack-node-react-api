663a2b48b5c20ae90bd386f02a3e82e95a13fa67
Hirano <hnaoya1120@gmail.com>
2022-12-03 09:30:41 +0900

    修正

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const app = express();
 const db = require("./models");
 
 app.use(cors());
-
 // クライアントから送信されたデータを、 req.body 経由で会得、操作できる。Body-Parser を基にExpressに組み込まれた機能、
 app.use(express.json());
 // Content-Type が application/x-www-form-urlencoded である POST リクエストのボディ部を解析し、 リクエストオブジェクトの body プロパティにフォームデータの内容を表すオブジェクトをセット
