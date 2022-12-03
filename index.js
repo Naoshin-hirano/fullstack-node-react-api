24f0ca6d7c6791e441eace7855e0cbf54c73a428
Hirano <hnaoya1120@gmail.com>
2022-12-03 13:05:26 +0900

    corsの設定を追加

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ require("dotenv").config();
 const app = express();
 const db = require("./models");
 
-app.use(cors());
+app.use(
+    cors({
+        origin: process.env.BASE_URL,
+        optionsSuccessStatus: 200,
+    })
+);
 // クライアントから送信されたデータを、 req.body 経由で会得、操作できる。Body-Parser を基にExpressに組み込まれた機能、
 app.use(express.json());
 // Content-Type が application/x-www-form-urlencoded である POST リクエストのボディ部を解析し、 リクエストオブジェクトの body プロパティにフォームデータの内容を表すオブジェクトをセット
